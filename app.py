3f2c2de904032b7b926f10f0fb7da96a454acdba
Priyadharshini <priyadharshini.b2024b@vitstudent.ac.in>
2026-09-22 05:50:11 +0530

    Add initial Flask application

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Student Assignment Submission & Similarity Tracking System"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
