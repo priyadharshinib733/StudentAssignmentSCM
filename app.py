65adde46ccb8924a63de8288686bc6ba7b490829
Priyadharshini <priyadharshini.b2024b@vitstudent.ac.in>
2026-09-22 06:06:55 +0530

    Implement student and faculty login

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "Student Assignment Submission & Similarity Tracking System"
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+        role = request.form["role"]
+
+        # Temporary login credentials
+        if role == "student" and username == "student" and password == "1234":
+            return "Welcome Student!"
+
+        if role == "faculty" and username == "faculty" and password == "1234":
+            return "Welcome Faculty!"
+
+        return render_template("login.html", error="Invalid username or password")
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
